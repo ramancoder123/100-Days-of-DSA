c336c3a92155bb0573f8685e6cef8e1a299c8378
ramancoder123 <ramandeepsingh45972@gmail.com>
2024-06-21 22:50:34 +0530

    Day36

---
+++ Day36/mergeKsorted.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+// C++ program to merge k sorted arrays of size n each
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node)
+{
+	while (node != NULL)
+	{
+		printf("%d ", node->data);
+		node = node->next;
+	}
+	cout<<endl;
+}
+
+// } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+	int data;
+	Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/ 
+
+class Solution{
+  public:
+    //Function to merge K sorted linked list.
+    Node * mergeKLists(Node *arr[], int K){
+        vector<int> ans;
+        for(int i = 0;i<K;i++){
+            Node* temp = arr[i];
+            while(temp != NULL){
+                ans.push_back(temp -> data);
+                temp = temp -> next;
+            }
+        }
+        
+        sort(ans.begin(), ans.end());
+        
+        Node* head = new Node(ans[0]);
+        Node* tail = head;
+        
+        for(int i = 1;i<ans.size();i++){
+            Node* temp = new Node(ans[i]);
+            tail -> next = temp;
+            tail = temp;
+        }
+        
+        return head;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+// Driver program to test above functions
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+	   int N;
+	   cin>>N;
+       struct Node *arr[N];
+       for(int j=0;j<N;j++)
+        {
+           int n;
+           cin>>n;
+
+           int x;
+           cin>>x;
+           arr[j]=new Node(x);
+           Node *curr = arr[j];
+           n--;
+
+           for(int i=0;i<n;i++)
+           {
+               cin>>x;
+               Node *temp = new Node(x);
+               curr->next =temp;
+               curr=temp;
+           }
+   		}
+   		Solution obj;
+   		Node *res = obj.mergeKLists(arr,N);
+		printList(res);
+
+   }
+
+	return 0;
+}
+
+// } Driver Code Ends
