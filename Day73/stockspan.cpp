6f41c7eb644363bd91073f653748d642f828e5b8
ramancoder123 <ramandeepsingh45972@gmail.com>
2024-08-03 19:16:00 +0530

    Day 73

---
+++ Day73/stockspan.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int arr[], int n){
+        vector<int> ans;
+        stack<int> st;
+        
+        for(int i = 0;i<n;i++){
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans.push_back(i+1);
+            }
+            else{
+                ans.push_back(i - st.top());
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
